627a44dd768b735e5f19f17e7ddb0405a0fd4574
Kamal Shrestha <kshrestha1@godaddy.com>
2025-04-26 15:33:56 +0545

    init

--- apps/web/app/api/users/route.ts
+++ apps/web/app/api/users/route.ts
@@ -3,8 +3,13 @@ import prisma from "@repo/db/client";
 
 export async function GET() {
   try {
+    
     const users = await prisma.user.findMany();
+    
+    
     return NextResponse.json(users);
+
+
   } catch (error) {
     console.error("Error fetching users:", error);
     return NextResponse.json(
