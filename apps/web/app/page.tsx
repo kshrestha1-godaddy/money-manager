29723f543f1635c39ba7dbe9a80c011725f4eab7
Kamal Shrestha <kshrestha1@godaddy.com>
2025-04-26 14:54:04 +0545

    init

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,44 @@
+import { Card } from "@repo/ui/card";
+
+const LINKS = [
+  {
+    title: "Docs",
+    href: "https://turborepo.com/docs",
+    description: "Find in-depth information about Turborepo features and API.",
+  },
+  {
+    title: "Learn",
+    href: "https://turborepo.com/docs/handbook",
+    description: "Learn more about monorepos with our handbook.",
+  },
+  {
+    title: "Templates",
+    href: "https://turborepo.com/docs/getting-started/from-example",
+    description: "Choose from over 15 examples and deploy with a single click.",
+  },
+  {
+    title: "Deploy",
+    href: "https://vercel.com/new",
+    description:
+      "Instantly deploy your Turborepo to a shareable URL with Vercel.",
+  },
+];
+
 export default function Page() {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-green-400">
       <h1>Hello World from web</h1>
+
+
+      <div className="grid mb-32 text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+        {LINKS.map(({ title, href, description }) => (
+          <Card href={href} key={title} title={title}>
+            {description}
+          </Card>
+        ))}
+      </div>
+
+
     </div>
   );
 }
