0ea1d11815ed4136a280ff9832d9fdbf88f3e16f
Kamal Shrestha <kshrestha1@godaddy.com>
2025-04-26 19:39:06 +0545

    init

--- apps/web/app/components/Appbar.tsx
+++ apps/web/app/components/Appbar.tsx
@@ -19,7 +19,7 @@ export const AppbarComponent = ({ user, onSignin, onSignout }: AppbarProps) => {
     return (
         <div className="flex justify-between border-b px-4 border-slate-300">
             <div className="text-lg flex flex-col justify-center">PayTM</div>
-            <div className="text-lg flex flex-col justify-center">{user?.name} - {balance}</div>
+            <div className="text-lg flex flex-col justify-center">{user?.name} - Rs. {balance}</div>
 
             <div className="flex flex-col justify-center pt-2">
                 <Button onClick={user ? onSignout : onSignin}>
