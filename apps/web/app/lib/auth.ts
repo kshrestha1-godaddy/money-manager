e7fe98a9bc9593a7beab6030d86ad69dc0f43b2d
Kamal Shrestha <kshrestha1@godaddy.com>
2025-06-13 20:07:15 +0545

    removed google oauth

--- apps/web/app/lib/auth.ts
+++ apps/web/app/lib/auth.ts
@@ -49,10 +49,10 @@ export const authOptions = {
         }
       },
     }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
+    // GoogleProvider({
+    //   clientId: process.env.GOOGLE_CLIENT_ID as string,
+    //   clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    // }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
   
