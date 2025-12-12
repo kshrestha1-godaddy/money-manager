a1f8655978aae4809fea7727b6cc1b4cafcb538b
Kamal Shrestha <kshrestha1@godaddy.com>
2025-12-12 09:53:02 +0545

    logo

--- apps/web/app/components/NavBar.tsx
+++ apps/web/app/components/NavBar.tsx
@@ -72,7 +72,7 @@ export default function NavBar() {
           {/* App Logo and Info Container */}
           <Link href={status === "authenticated" ? "/dashboard" : "/"} className="flex items-center space-x-4">
             <Image 
-              src="/logo-zero.gif" 
+              src="/logo.png" 
               alt="Money Manager Logo" 
               priority={true}
               width={56} 
