bc55b6b3099c2d373b426af6356f0470259bba76
Kamal Shrestha <kshrestha1@godaddy.com>
2025-12-12 10:14:25 +0545

    logo

--- apps/web/app/components/NavBar.tsx
+++ apps/web/app/components/NavBar.tsx
@@ -158,6 +158,17 @@ export default function NavBar() {
                   </svg>
                 </Link>
               )}
+
+              {/* Thoughts icon link */}
+              {status === "authenticated" && (
+                <Link href="/notes" className="p-2 rounded-full hover:bg-gray-100 transition-colors" aria-label="Thoughts">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={`w-8 h-8 ${pathname?.startsWith('/notes') ? 'text-blue-600' : 'text-gray-500'}`}>
+                    <path d="M11.644 1.59a.75.75 0 0 1 .712 0l9.75 5.25a.75.75 0 0 1 0 1.32l-9.75 5.25a.75.75 0 0 1-.712 0l-9.75-5.25a.75.75 0 0 1 0-1.32l9.75-5.25Z" />
+                    <path d="m3.265 10.602 7.668 4.129a2.25 2.25 0 0 0 2.134 0l7.668-4.13 1.37.739a.75.75 0 0 1 0 1.32l-9.75 5.25a.75.75 0 0 1-.71 0l-9.75-5.25a.75.75 0 0 1 0-1.32l1.37-.738Z" />
+                    <path d="m10.933 19.231-7.668-4.13-1.37.739a.75.75 0 0 0 0 1.32l9.75 5.25c.221.12.489.12.71 0l9.75-5.25a.75.75 0 0 0 0-1.32l-1.37-.738-7.668 4.13a2.25 2.25 0 0 1-2.134-.001Z" />
+                  </svg>
+                </Link>
+              )}
               {/* Notification Bell */}
               <NotificationBell className="mr-2" />
               
